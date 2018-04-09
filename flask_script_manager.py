742aed0cd49bb6e8e0b2ef33a3096899f821f1fa
leoluyi <leo0650@gmail.com>
2018-04-09 22:52:59 +0800

    Add flask-cli info

--- flask_script_manager.py
+++ flask_script_manager.py
@@ -1,9 +1,16 @@
 # hello.py
 
+'''Warning
+
+While the maintainers are willing to merge PR’s, they are not actively 
+developing features. As of Flask 0.11, Flask includes a built-in CLI tool, and
+that may fit your needs better.
+
+http://flask.pocoo.org/docs/0.12/cli/
+'''
+
 from flask import Flask
 from flask_script import Manager
-# from flask.ext.script import Manager
-
 
 app = Flask(__name__)
 manager = Manager(app)
