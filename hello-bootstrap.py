f0666ddc6bc6f19ea2540b6d86a84aed8fc73b7c
leoluyi <leo0650@gmail.com>
2018-04-09 21:54:59 +0800

    Add notes

--- hello-bootstrap.py
+++ hello-bootstrap.py
@@ -8,6 +8,9 @@ bootstrap = Bootstrap(app)
 
 @app.route("/user/<name>")
 def user(name):
+    # By default, flask looks for front-end code in a subdirectory called
+    # 'templates' located in the root folder.
+    # You can change default folder in Flask() options
     return render_template("user.html", name=name)
 
 
